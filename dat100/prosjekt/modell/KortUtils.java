e67eccac7c99fbdc8c4e200c9226ac138cbd35b9
JonasRygg <52327780+JonasRygg@users.noreply.github.com>
2023-10-16 22:09:03 +0200

    Add files via upload
    dawd

---
+++ dat100/prosjekt/modell/KortUtils.java
@@ -0,0 +1,39 @@
+package no.hvl.dat100.prosjekt.modell;
+
+import no.hvl.dat100.prosjekt.TODO;
+import no.hvl.dat100.prosjekt.kontroll.Kort;
+
+public class KortUtils {
+
+	/**
+	 * Sorterer en samling. Rekkefølgen er bestemt av compareTo() i Kort-klassen.
+	 * 
+	 * @see Kort
+	 * 
+	 * @param samling
+	 * 			samling av kort som skal sorteres. 
+	 */
+	
+	public static void sorter(KortSamling samling) {
+		
+		// TODO - START
+		
+		throw new UnsupportedOperationException(TODO.method());
+		// TODO - END
+	}
+	
+	/**
+	 * Stokkar en kortsamling. 
+	 * 
+	 * @param samling
+	 * 			samling av kort som skal stokkes. 
+	 */
+	public static void stokk(KortSamling samling) {
+		
+		// TODO - START
+		
+		throw new UnsupportedOperationException(TODO.method());
+		// TODO - END
+	}
+	
+}
